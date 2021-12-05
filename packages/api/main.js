f459aa9ee676720c345ef3493f2b18b47dbe75ca
John Brunton <john_brunton@live.co.uk>
2021-12-05 17:04:04 +0000

    implement get counter endpoint

--- packages/api/main.js
+++ packages/api/main.js
@@ -1,9 +1,37 @@
-const express = require('express')
+const { response } = require('express');
+const express = require('express');
+const knex = require('./db/knex');
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const db = require('./db/knex');
+
+app.get('/counters/:sessionId', async (req, res) => {
+  try {
+    const sessionId = req.params.sessionId;
+    if (!sessionId) {
+      res.status(422).send("Missing sessionId");
+      return;
+    }
+    const [counter] = await db.transaction(async trx => {
+      const counters = await trx.select('*')
+        .from('counters')
+        .where('session_id', sessionId);
+
+      if (counters.length === 0) {
+        return knex.transacting(trx)
+          .insert({ session_id: sessionId, count: 0 })
+          .returning("*");
+      }
+
+      return counters;
+    });
+    
+    res.json(counter);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 })
 
 app.listen(port, () => {

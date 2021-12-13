c9241f4c9dfca87d14d4a807ee478d83b3064615
John Brunton <john_brunton@live.co.uk>
2021-12-13 22:46:38 +0000

    feat: simple counter SPA

--- packages/api/main.js
+++ packages/api/main.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 3001
 const countersRouter = require('./routers/counters');
 
+const cors = require('cors');
+app.use(cors());
 app.use("/counters", countersRouter);
 
 app.listen(port, () => {
--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,21 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+
+const newSessionId = () => (Math.random() + 1).toString(36).substring(7);
+
+const getCounter = async (sessionId) => {
+  const response = await fetch(`http://localhost:3001/counters/${sessionId}`);
+  const counter = await response.json();
+  return counter;
+}
+
+const incrementCounter = async (sessionId) => {
+  await fetch(`http://localhost:3001/counters/${sessionId}/increment`, { method: "PUT" });
+  return await getCounter(sessionId);
+}
+
+const resetCounter = async (sessionId) => {
+  await fetch(`http://localhost:3001/counters/${sessionId}`, { method: "DELETE" });
+  return await getCounter(sessionId);
+}
 
 function App() {
+  const [sessionId] = useState(newSessionId());
+  const [count, setCount] = useState(0);
+  
+  useEffect(async () => {
+    const counter = await getCounter(sessionId);
+    setCount(counter.count);
+  });
+
+  const incrementCounterClicked = async () => {
+    const counter = await incrementCounter(sessionId);
+    setCount(counter.count);
+  };
+
+  const resetCounterClicked = async () => {
+    const counter = await resetCounter(sessionId);
+    setCount(counter.count);
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Session ID: <code>{sessionId}</code>
+        </p>
+        <p>
+          Counter: <code>{count}</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
+          onClick={incrementCounterClicked}
         >
-          Learn React
+          Increment
+        </a>
+        <a
+          className="App-link"
+          href="#"
+          onClick={resetCounterClicked}
+        >
+          Reset
         </a>
       </header>
     </div>

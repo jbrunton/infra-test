4e15234757fb3f819ec8ab6362dcd7f93cf817ea
John Brunton <john_brunton@live.co.uk>
2021-12-13 22:19:52 +0000

    refactor: rename controllers -> routers

--- packages/api/main.js
+++ packages/api/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const port = 3001
-const countersRouter = require('./controllers/counters');
+const countersRouter = require('./routers/counters');
 
 app.use("/counters", countersRouter);
 

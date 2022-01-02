7290b657dcef5aaff08dda050e4a72d3137ea04f
John Brunton <john_brunton@live.co.uk>
2022-01-02 15:49:19 +0000

    refactor: pulumi config

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,35 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as digitalocean from "@pulumi/digitalocean";
-
-const userData =
-`#cloud-config
-runcmd:
-  # install docker-compose: https://docs.docker.com/compose/install/
-  - sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  - sudo chmod +x /usr/local/bin/docker-compose
-  # install Carvel. TODO: pin versions.
-  - wget -O- https://carvel.dev/install.sh | bash
-
-apt:
-  sources:
-    docker.list:
-      source: deb [arch=amd64] https://download.docker.com/linux/ubuntu $RELEASE stable
-      keyid: 9DC858229FC7DD38854AE2D88D81803C0EBFCD88
-
-packages:
-  - docker-ce
-  - docker-ce-cli
-  # increase system entropy per https://github.com/docker/compose/issues/6678
-  - haveged
-`
-
-const awsProvider = new aws.Provider("aws", { region: "eu-west-1" });
+import * as fs from 'fs';
 
 const stackName = pulumi.getStack();
 
+// Create the droplet
+const userData = fs.readFileSync("./user_data.yml", 'utf-8');
 const sshKey = digitalocean.getSshKey({ name: "macbook-2020-id_ed25519" });
-
 const droplet = new digitalocean.Droplet("test-droplet", {
   image: "ubuntu-20-04-x64",
   name: `test-droplet-${stackName}`,
@@ -39,11 +17,11 @@ const droplet = new digitalocean.Droplet("test-droplet", {
   userData: userData,
 });
 
-// Export the name of the bucket
 export const ip = droplet.ipv4Address;
 
+// Create DNS record
+const awsProvider = new aws.Provider("aws", { region: "eu-west-1" });
 const zone = aws.route53.getZone({ name: "jbrunton-aws.com" }, { provider: awsProvider });
-
 const route53record = new aws.route53.Record("www", {
   zoneId: zone.then(zone => zone.zoneId),
   name: zone.then(zone => `test-droplet-${stackName}.infra-test.${zone.name}`),
@@ -51,3 +29,5 @@ const route53record = new aws.route53.Record("www", {
   records: [ip],
   ttl: 300,
 }, { provider: awsProvider });
+
+export const url = route53record.name;

a41ddea5bec3cc8cf46b6e4cf7fa4d7293bc372a
John Brunton <john_brunton@live.co.uk>
2022-03-12 14:14:25 +0000

    Pulumi file for API and web services

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,5 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as digitalocean from "@pulumi/digitalocean";
+import * as awsx from "@pulumi/awsx";
+import * as aws from "@pulumi/aws";
 import * as fs from "fs";
 import * as YAML from 'yaml'
 import {
@@ -10,6 +11,7 @@ import {
 
 const stackName = pulumi.getStack();
 const appName = `infra-test-${stackName}`;
+const provider = new aws.Provider("aws", { region: "eu-west-2" });
 
 type ServiceName = "api" | "web";
 
@@ -62,111 +64,130 @@ const readManifest = (): VersionManifest => {
 
 const manifest = readManifest();
 
-const apiEnvs: AppSpecStaticSiteEnv[] = [{
-  key: "POSTGRES_HOST",
-  scope: "RUN_TIME",
-  value: "${db.HOSTNAME}",
-}, {
-  key: "POSTGRES_PORT",
-  scope: "RUN_TIME",
-  value: "${db.PORT}",
-}, {
-  key: "POSTGRES_USER",
-  scope: "RUN_TIME",
-  value: "${db.USERNAME}",
-}, {
-  key: "POSTGRES_PASSWORD",
-  scope: "RUN_TIME",
-  value: "${db.PASSWORD}",
-}, {
-  key: "POSTGRES_DB",
-  scope: "RUN_TIME",
-  value: "${db.DATABASE}",
-}, {
-  key: "POSTGRES_CA_CERT",
-  scope: "RUN_TIME",
-  value: "${db.CA_CERT}",
-}];
-
-const serviceSettings: Record<ServiceName, AppSpecService> = {
-  "api": {
-    name: "api",
-    httpPort: 3001,
-    image: {
-      registry: "jbrunton",
-      registryType: "DOCKER_HUB",
-      repository: "infra-test_api",
-      tag: manifest.services["api"].version,
-    },
-    envs: apiEnvs,
-    instanceCount: manifest.services["api"].instanceCount,
-    instanceSizeSlug: manifest.services["api"].instanceSizeSlug,
-    routes: [{
-        path: "/api",
-    }],
-  },
-  "web": {
-    name: "web",
-    httpPort: 3000,
-    image: {
-      registry: "jbrunton",
-      registryType: "DOCKER_HUB",
-      repository: "infra-test_web",
-      tag: manifest.services["web"].version,
-    },
-    envs: [{
-      key: "REACT_APP_API_ADDRESS",
-      scope: "RUN_TIME",
-      value: "${APP_URL}/api",
-    }],
-    instanceCount: manifest.services["web"].instanceCount,
-    instanceSizeSlug: manifest.services["web"].instanceSizeSlug,
-    routes: [{
-        path: "/",
-    }],
+// Create a load balancer on port 80 and spin up two instances of Nginx.
+const cluster = new awsx.ecs.Cluster("infra-test-dev", {
+}, { provider });
+const loadBalancer = new awsx.lb.ApplicationLoadBalancer("infra-test-dev", {}, { provider });
+const listener = new awsx.lb.ApplicationListener("infra-test-dev", {
+  port: 80,
+  loadBalancer,
+  defaultAction: {
+    type: "fixed-response",
+    fixedResponse: {
+      statusCode: "503",
+      contentType: "text/plain",
+    }
   }
-};
+}, { provider });
 
-const services: AppSpecService[] = [];
-const jobs: AppSpecJob[] = [];
+const targetGroupWeb = new awsx.lb.ApplicationTargetGroup(`infra-test-dev-web`, {
+  port: 80,
+  protocol: "HTTP",
+  vpc: cluster.vpc,
+  loadBalancer
+}, { provider });
 
-(['api', 'web'] as ServiceName[]).forEach((serviceName: ServiceName) => {
-  const serviceManifest = manifest.services[serviceName];
-  const service = serviceSettings[serviceName];
-  services.push(service);
-
-  if (serviceManifest.jobs) {
-    serviceManifest.jobs.forEach(jobManifest => {
-      const job: AppSpecJob = {
-        name: jobManifest.name,
-        kind: jobManifest.kind,
-        image: service.image,
-        envs: service.envs,
-        runCommand: jobManifest.runCommand,
-        instanceCount: 1,
-        instanceSizeSlug: jobManifest.instanceSizeSlug,
-      };
-      jobs.push(job);
-    });
+const targetGroupApi = new awsx.lb.ApplicationTargetGroup(`infra-test-dev-api`, {
+  port: 80,
+  protocol: "HTTP",
+  vpc: cluster.vpc,
+  loadBalancer,
+  healthCheck: {
+    path: "/health"
   }
-});
+}, { provider });
 
-new digitalocean.App(appName, {
-  spec: {
-    name: appName,
-    region: "lon",
-    databases: [{
-        engine: "PG",
-        name: "db",
-        production: false,
-        version: "12",
-    }],
-    domainNames: [{
-        name: domainName,
-        zone: "jbrunton-do.com",
-        type: "PRIMARY"
-    }],
-    services,
-    jobs,
+listener.addListenerRule("web", {
+  actions: [{
+    type: "forward",
+    targetGroupArn: targetGroupWeb.targetGroup.arn
+  }],
+  conditions: [
+    {
+      hostHeader: {
+          values: [`app.infra-test.dev.jbrunton-aws.com`],
+      },
   },
-});
+  ]
+}, { provider });
+
+listener.addListenerRule("api", {
+  actions: [{
+    type: "forward",
+    targetGroupArn: targetGroupApi.targetGroup.arn
+  }],
+  conditions: [
+    {
+      hostHeader: {
+          values: [`api.infra-test.dev.jbrunton-aws.com`],
+      },
+  },
+  ]
+}, { provider });
+
+const web = new awsx.ecs.FargateService("web", {
+  cluster,
+  loadBalancers: [{
+    containerName: "web",
+    containerPort: 80,
+    targetGroupArn: targetGroupWeb.targetGroup.arn
+  }],
+  taskDefinitionArgs: {
+    containers: {
+      web: {
+        image: "jbrunton/infra-test_web",
+        memory: 256,
+        portMappings: [{
+          containerPort: 80,
+          hostPort: 80
+        }],
+        environment: [
+          { name: "WEB_PORT", value: "80" },
+          { name: "REACT_APP_API_ADDRESS", value: "http://api.infra-test.dev.jbrunton-aws.com" }
+        ]
+      },
+    },
+  },
+  desiredCount: 1,
+}, { provider });
+
+const api = new awsx.ecs.FargateService("api", {
+  cluster,
+  loadBalancers: [{
+    containerName: "api",
+    containerPort: 80,
+    targetGroupArn: targetGroupApi.targetGroup.arn
+  }],
+  taskDefinitionArgs: {
+    containers: {
+      api: {
+        image: "jbrunton/infra-test_api",
+        memory: 256,
+        portMappings: [{
+          containerPort: 80,
+          hostPort: 80
+        }],
+        environment: [
+          { name: "API_PORT", value: "80" }
+        ]
+      },
+    },
+  },
+  desiredCount: 1,
+}, { provider });
+
+
+
+// // Export the load balancer's address so that it's easy to access.
+export const url = listener.endpoint.hostname;
+
+const www = new aws.route53.Record("www", {
+  zoneId: aws.route53.getZone({ name: "jbrunton-aws.com" }, { provider }).then(zone => zone.id),
+  name: "*.infra-test.dev.jbrunton-aws.com",
+  type: "A",
+  aliases: [{
+      name: loadBalancer.loadBalancer.dnsName,
+      zoneId: loadBalancer.loadBalancer.zoneId,
+      evaluateTargetHealth: true,
+  }],
+}, { provider });
